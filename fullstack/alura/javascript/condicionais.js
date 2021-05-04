68503f9109ddca48c4112471e55e971ad1866682
Gustavo de Matos Parizi <gusparizi@gmail.com>
2021-05-04 15:30:07 -0300

    atualizando arquivos curso javascript

--- fullstack/alura/javascript/condicionais.js
+++ fullstack/alura/javascript/condicionais.js
@@ -9,9 +9,54 @@ const listaDeDestinos = new Array(
     `Curitiba`,
 );
 
+const idadeComprador = 19;
+const estaAcompanhada = true;
+const temPassagemComprada = true;
+
+/*
+
+if(idadeComprador >= 18){
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1, 1);
+} else{
+    // a pessoa é menor de idade
+    if(estaAcompanhada == true){
+        console.log("Comprador está acompanhado");
+        listaDeDestinos.splice(1, 1); 
+    }
+    console.log("Não é maior de idade e não posso vender")
+};
+
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-// removendo um item da lista
+*/
 
-listaDeDestinos.splice(1, 1);
+/* outra forma de codificar o if */
+
+// if(idadeComprador >= 18){
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1);
+// } else if(estaAcompanhada == true){
+//         console.log("Comprador está acompanhado");
+//         listaDeDestinos.splice(1, 1); 
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// };
+
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem!");
+    listaDeDestinos.splice(1, 1);
+} else {
+    console.log("Não é maior de idade e não posso vender");
+};
+
+console.log("Embarque: \n\n")
+if (idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem!");
+} else {
+    console.log("Você não pode embarcar.")
+}
+
+console.log(listaDeDestinos);
